2f180efc2148c5911eb3bcdd6242cf42368ed0c8
ArlethKarinaAcosta <43972814+ArlethKarinaAcosta@users.noreply.github.com>
2019-02-24 20:26:08 -0700

    Cursor RecyclerView

---
+++ Unidad1/Mydb03/app/src/main/java/com/ejemplo/mydb03/entidades/Usuario.java
@@ -0,0 +1,44 @@
+package com.ejemplo.mydb03.entidades;
+
+/**
+ * Created by lenovo on 27/02/2018.
+ */
+
+
+//Esta clase es para la manipulación de datos.
+    //Definición de constantes para los alias de los campos.
+public class Usuario {
+    private int Clave;
+    private String nombre;
+    private double sueldo;
+
+    public Usuario(int clave, String nombre, double sueldo) {
+        Clave = clave;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public int getClave() {
+        return Clave;
+    }
+
+    public void setClave(int clave) {
+        Clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+}
